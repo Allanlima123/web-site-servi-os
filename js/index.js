6f587d77a321bc8176f4e4fe784e58a8235faabf
alan lima <alan.silva1f@gmail.com>
2021-11-26 21:25:40 -0300

    commitando a seção about

--- js/index.js
+++ js/index.js
@@ -7,5 +7,11 @@ $(document).ready(function(){
     $(window).on('load scroll',function(){
         $('.fa-bars').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 30){
+            $('.cabecalho').css({'background':'#6C5CE7','box-shadow':'0 .2re .5rem rgba(0,0,0,.4'});
+        }else{
+            $('.cabecalho').css({'background':'none','box-shadow':'none'});
+        }
     });
 });
